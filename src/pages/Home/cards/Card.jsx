24dfcdd702e0235880d58a9cd4d67c6db7d1d7b7
AlamRazek <ohidul933gmail.com>
2023-10-21 05:42:30 +0100

    update form added for updating product

--- src/pages/Home/cards/Card.jsx
+++ src/pages/Home/cards/Card.jsx
@@ -1,21 +1,14 @@
-import { useEffect, useState } from "react";
-import { Link, useLoaderData, useParams } from "react-router-dom";
-import CardDetails from "./CardDetails";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({ card }) => {
   const { name, photo } = card;
-  const { brandName } = useParams();
   const [brand, setBrand] = useState(name);
-
-  const cardDetails = (name) => {};
 
   return (
     <>
       <Link to={`/cardDetails/${brand}`}>
-        <div
-          className="card  max-h-[414px] p-2 bg-gray-200"
-          onClick={() => cardDetails(name)}
-        >
+        <div className="card  max-h-[414px] p-2 bg-gray-200">
           <figure className="max-h-[300px] rounded-md">
             <img src={photo} alt="" />
           </figure>
@@ -23,7 +16,6 @@ const Card = ({ card }) => {
             <h2 className="card-title font-bold ">{name}</h2>
           </div>
         </div>
-        {}
       </Link>
     </>
   );
